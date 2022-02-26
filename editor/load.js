09ccd36ddcd0505a3656634d1b87e1020ad47aa1
codewyx <66934040+codewyx@users.noreply.github.com>
2022-02-26 11:49:42 +0800

    Update load.js

--- editor/load.js
+++ editor/load.js
@@ -49,7 +49,7 @@ function saveHtml() {
 }
 function showabout() {
   mdui.snackbar({
-    message: "Rapid ：Version 0.1.5",
+    message: "Rapid ：Version 0.1.6",
     position: "right-top"
   });  
 	
