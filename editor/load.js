eccee5c5f906e5269854609b7be6a9259ff21531
codewyx <66934040+codewyx@users.noreply.github.com>
2022-03-06 11:35:10 +0800

    Update load.js

--- editor/load.js
+++ editor/load.js
@@ -9,7 +9,22 @@ $('#open').on('click', function () {
 });
 
 var demoWorkspace = Blockly.inject('blocklyDiv', {toolbox: document.getElementById('toolbox'),grid: {spacing: 40,length: 3,colour: '#ccc',snap: true},});Blockly.Xml.domToWorkspace(document.getElementById('startBlocks'), demoWorkspace);
+// 1. 为了创建自定义类别，先创建自定义类别，继承自Blockly.ToolboxCategory
+class CustomCategory extends Blockly.ToolboxCategory {
+    // 自定义类别创造函数
+    // categoryDef: 类别定义的信息
+    // toolbox: 表示类别的父级toolbox
+    // opt_parent: 可选参数，表示其父类别
+    constructor(categoryDef, toolbox, opt_parent) {
+        super(categoryDef, toolbox, opt_parent);
+    }
+}
 
+// 2. 自定义类别需要向Blockly注册，告知自定义类别的存在，不然会无法识别新建的类
+Blockly.registry.register(
+    Blockly.registry.Type.TOOLBOX_ITEM,
+    Blockly.ToolboxCategory.registrationName,
+    CustomCategory, true);
 
 		function showHtml() {
   Blockly.PHP.INFINITE_LOOP_TRAP = true;
