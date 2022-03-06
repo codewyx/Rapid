346427cbaa4532d3980f4b262f1f145c07ec6ed8
codewyx <66934040+codewyx@users.noreply.github.com>
2022-03-06 12:05:46 +0800

    Update load.js

--- editor/load.js
+++ editor/load.js
@@ -13,7 +13,7 @@ $('#open').on('click', function () {
 function start() {}
 
 
-	  var demoworkspace = Blockly.inject('blocklyDiv', {toolbox: document.getElementById('toolbox'),grid: {spacing: 40,length: 3,colour: '#ccc',snap: true},});Blockly.Xml.domToWorkspace(document.getElementById('startBlocks'), demoWorkspace);
+	  var demoworkspace = Blockly.inject('blocklyDiv', {toolbox: document.getElementById('toolbox'),grid: {spacing: 40,length: 3,colour: '#ccc',snap: true},})
 
 
 		function showHtml() {
