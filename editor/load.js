7017668a2d88efbee3f320e809a693ce96937ed2
codewyx <66934040+codewyx@users.noreply.github.com>
2022-03-08 13:20:22 +0800

    Update load.js

--- editor/load.js
+++ editor/load.js
@@ -12,58 +12,7 @@ $('#open').on('click', function () {
 
 function start() {}
 
-
-	  var demoworkspace = Blockly.inject('blocklyDiv', {toolbox: document.getElementById('toolbox'),grid: {spacing: 40,length: 3,colour: '#ccc',snap: true},})
-
-
-
-class CustomCategory extends Blockly.ToolboxCategory {
-
-  constructor(categoryDef, toolbox, opt_parent) {
-    super(categoryDef, toolbox, opt_parent);
-  }
-
-  addColourBorder_(colour){
-    this.rowDiv_.style.backgroundColor = colour;
-  }
-
- 
-  setSelected(isSelected){
-    // We do not store the label span on the category, so use getElementsByClassName.
-    var labelDom = this.rowDiv_.getElementsByClassName('blocklyTreeLabel')[0];
-    if (isSelected) {
-      // Change the background color of the div to white.
-      this.rowDiv_.style.backgroundColor = 'white';
-      // Set the colour of the text to the colour of the category.
-      labelDom.style.color = this.colour_;
-      this.iconDom_.style.color = this.colour_;
-    } else {
-      // Set the background back to the original colour.
-      this.rowDiv_.style.backgroundColor = this.colour_;
-      // Set the text back to white.
-      labelDom.style.color = 'white';
-      this.iconDom_.style.color = 'white';
-    }
-    // This is used for accessibility purposes.
-    Blockly.utils.aria.setState(/** @type {!Element} */ (this.htmlDiv_),
-        Blockly.utils.aria.State.SELECTED, isSelected);
-  }
-
-
-  createIconDom_() {
-    const iconImg = document.createElement('img');
-    iconImg.src = './logo_only.svg';
-    iconImg.alt = 'Blockly Logo';
-    iconImg.width = '25';
-    iconImg.height = '25';
-    return iconImg;
-  }
-}
-
-Blockly.registry.register(
-    Blockly.registry.Type.TOOLBOX_ITEM,
-    Blockly.ToolboxCategory.registrationName,
-    CustomCategory, true);
+var demoWorkspace = Blockly.inject('blocklyDiv', {toolbox: document.getElementById('toolbox'),grid: {spacing: 40,length: 3,colour: '#ccc',snap: true},});Blockly.Xml.domToWorkspace(document.getElementById('startBlocks'), demoWorkspace);
 
 		function showHtml() {
   Blockly.PHP.INFINITE_LOOP_TRAP = true;
