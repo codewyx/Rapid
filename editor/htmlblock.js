ae531b70897b640d4fc59fd5862d5ba0b900e1a4
codewyx <66934040+codewyx@users.noreply.github.com>
2022-03-26 09:50:50 +0800

    Update htmlblock.js

--- editor/htmlblock.js
+++ editor/htmlblock.js
@@ -335,20 +335,7 @@ Blockly.Blocks['1link'] = {
         this.setNextStatement(true, null);
   }
 };
-Blockly.Blocks['textx'] = {
-    init: function () {
-        this.appendDummyInput()
-            .appendField(blan['textx01']);
-        .appendField(new Blockly.FieldDropdown([["定义粗体文本","1"], ["定义大号字","2"], ["定义着重文字","3"],["定义斜体字","4"],["定义小号字","5"],["定义加重语气","6"],["定义下标字","7"],["定义上标字","8"],["定义插入字","9"],["定义删除字","10"]]), "textxk001");
-        this.appendStatementInput("textxk002")
-            .setCheck(null);
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(230);
-        this.setTooltip("");
-        this.setHelpUrl("");
-    }
-};
+
 //Blockly.Blocks['iframe'] = {
  //   init: function () {
    //     this.appendDummyInput()
